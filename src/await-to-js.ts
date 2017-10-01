fc562a677bec9c2328bdd9591c7049b164161bfe
Dima Grossman <dima@grossman.io>
2017-10-01 13:48:49 +0300

    refactor: Added typescript support and UMD support

---
+++ src/await-to-js.ts
@@ -0,0 +1,21 @@
+/**
+ * @param { Promise } promise
+ * @param { Object= } errorExt - Additional Information you can pass to the err object
+ * @return { Promise }
+ */
+export function to<T, U = any>(
+  promise: Promise<T>,
+  errorExt?: object
+): Promise<[U | null, T | undefined]> {
+  return promise
+    .then<[null, T]>((data: T) => [null, data])
+    .catch<[U, undefined]>(err => {
+      if (errorExt) {
+        Object.assign(err, errorExt)
+      }
+
+      return [err, undefined]
+    })
+}
+
+export default to
---
+++ test/await-to-js.test.ts
@@ -0,0 +1,38 @@
+import { to } from "../src/await-to-js"
+
+describe("Await to test", () => {
+  it("should return a value when resolved", async () => {
+    const testInput = 41
+    const promise = Promise.resolve(testInput)
+
+    const [err, data] = await to<number>(promise)
+
+    expect(err).toBeNull()
+    expect(data).toEqual(testInput)
+  })
+
+  it("should return an error when promise is rejected", async () => {
+    const testInput = 41
+    const promise = Promise.reject("Error")
+
+    const [err, data] = await to<number>(promise)
+
+    expect(err).toEqual("Error")
+    expect(data).toBeUndefined()
+  })
+
+  it("should add external properties to the error object", async () => {
+    const promise = Promise.reject({ error: "Error message" })
+
+    const [err] = await to<
+      string,
+      { error: string; extraKey: number }
+    >(promise, {
+      extraKey: 1
+    })
+
+    expect(err).toBeTruthy()
+    expect(err.extraKey).toEqual(1)
+    expect(err.error).toEqual("Error message")
+  })
+})
