e82a287cc191f787b2ccc157a7fce8c08ee78cb8
John Forster <j_forster@hotmail.co.uk>
2020-03-02 15:23:15 +0000

    Correct typings to use tuples.
    The previous type `() => [U | null, undefined | T]` is incorrect, as it allows for typings such as `[U, T]` which is an impossible result.
    
    Changing this type to be `() => [U, undefined] | [null, T]` correctly characterises the possible type results.

--- src/await-to-js.ts
+++ src/await-to-js.ts
@@ -3,10 +3,10 @@
  * @param { Object= } errorExt - Additional Information you can pass to the err object
  * @return { Promise }
  */
-export function to<T, U = Error> (
+export function to<T, U = Error>(
   promise: Promise<T>,
-  errorExt?: object
-): Promise<[U | null, T | undefined]> {
+  errorExt?: object,
+): Promise<[U, undefined] | [null, T]> {
   return promise
     .then<[null, T]>((data: T) => [null, data])
     .catch<[U, undefined]>((err: U) => {
