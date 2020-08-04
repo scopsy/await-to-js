cd88c54b680540c4ca5318667e85d9bb4635591c
Vít Stanislav <vits@kryptoslogic.com>
2020-08-04 14:49:58 +0200

    Make the type of the return value more precise
    If the first return value (error) is null, then this change implies that the second return value is not `undefined`.
    
    It solves a problem I was facing: getting `Type 'APIResponse<any> | undefined' is not assignable to type 'APIResponse<any>'` type error on the last row of the following Vuex (with vuex-module-decorators) example:
    ```js
        @Action({ rawError: true })
        public async deleteAsset(asset: Asset): Promise<APIResponse> {
          this.context.commit('setIsLoading', true);
          const [err, response] = await to<APIResponse, APIError>(deleteAsset(asset));
          this.context.commit('setIsLoading', false);
          if (err) {
            throw err;
          }   
          this.context.commit('setAssetDeleted', asset);
          return response;
        }
    ```

--- src/await-to-js.ts
+++ src/await-to-js.ts
@@ -6,7 +6,7 @@
 export function to<T, U = Error> (
   promise: Promise<T>,
   errorExt?: object
-): Promise<[U | null, T | undefined]> {
+): Promise<[U, undefined]> | Promise<[null, T]> {
   return promise
     .then<[null, T]>((data: T) => [null, data])
     .catch<[U, undefined]>((err: U) => {
