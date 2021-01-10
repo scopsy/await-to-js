8b53f36c95384d9b01f1b99819474d88d3b7e1ab
Dima Grossman <dima@grossman.io>
2021-01-10 10:49:30 +0200

    Apply suggestions from code review
    Co-authored-by: Karl Horky <karl.horky@gmail.com>

--- src/await-to-js.ts
+++ src/await-to-js.ts
@@ -6,7 +6,7 @@
 export function to<T, U = Error> (
   promise: Promise<T>,
   errorExt?: object
-): Promise<[U, undefined]> | Promise<[null, T]> {
+): Promise<[U, undefined] | [null, T]> {
   return promise
     .then<[null, T]>((data: T) => [null, data])
     .catch<[U, undefined]>((err: U) => {
